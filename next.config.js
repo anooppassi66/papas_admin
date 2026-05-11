76db776b22ddfa1f4d3f8d473392a9d0e4fca0bb
Anoop <anooppassi66@gmail.com>
2026-05-11 17:53:58 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ protocol: 'http', hostname: 'localhost', port: '5001' }],
+    remotePatterns: [{ protocol: 'http', hostname: 'localhost', port: '5001' },{ protocol: 'http', hostname: '52.64.40.205', port: '4003' }],
   },
 };
 
